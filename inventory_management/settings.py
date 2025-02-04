f0eac4de5d2fe5931d18b1ec5259b8d639b67eb2
sARASWAN-sINHA <saraswan1999@gmail.com>
2025-02-05 00:07:49 +0530

    adding swagger docs to apis

--- inventory_management/settings.py
+++ inventory_management/settings.py
@@ -46,6 +46,7 @@ DJANGO_CUSTOM_APPS = ["core", ]
 THIRD_PARTY_APPS = [
         "rest_framework",
         "djoser",
+        "drf_spectacular",
     ]
 
 INSTALLED_APPS = DJANGO_INBUILT_APPS + DJANGO_CUSTOM_APPS + THIRD_PARTY_APPS
@@ -141,6 +142,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'core.User'
 
 REST_FRAMEWORK = {
+
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
@@ -148,4 +151,11 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
 }
